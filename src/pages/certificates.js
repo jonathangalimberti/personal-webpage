8f877298163ec7cf34337ee20024a5ef9d98e3b2
jonathan <jonathangalimberti@hotmail.com>
2026-04-21 20:45:43 -0300

    Se agrega funcionalidades, filtros y actualización de certificados segun json

--- src/pages/certificates.js
+++ src/pages/certificates.js
@@ -1,9 +1,61 @@
-const url = "../data/certificates.json"
+const url = "../data/certificates.json";
+const container = document.getElementById("container");
+let certificates = [];
 
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    certificates = data;
+    printCertificates(data);
+  } catch (err) {
+    console.error("Error fetching data " + err);
+  }
+}
+
+fetchData();
+
+function printCertificates(data) {
+  container.innerHTML = "";
+  data.forEach((cert) => {
+    container.innerHTML += `<div class="card col-3 p-0">
+      <img class="card-img-top" src="${cert.img}" alt="Title" />
+    </div>
+    `;
   });
+}
 
- 
+function filter(data) {
+  container.innerHTML = "";
+
+  certificates.forEach((cert) => {
+    if (cert.category == data) {
+      container.innerHTML += `<div class="card col-3 p-0">
+      <img class="card-img-top" src="${cert.img}" alt="Title" />
+    </div>
+    `;
+      console.log(cert.category, data);
+    } else if (data === "All") {
+      printCertificates(certificates);
+    }
+  });
+  console.log(data, certificates);
+}
+
+function searchCertificate(value) {
+  container.innerHTML = "";
+  let valueString = value.toLowerCase();
+
+  for (let i = 0; i < certificates.length; i++) {
+    const element = certificates[i];
+    if (element.name.toLowerCase().includes(valueString)) {
+      container.innerHTML += `<div class="card col-3 p-0">
+      <img class="card-img-top" src="${element.img}" alt="Title" />
+    </div>
+    `;
+      console.log(element);
+    }
+  }
+
+  console.log(value);
+}
