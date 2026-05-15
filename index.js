b2fdd6db7c659dec519e97b4e1dce21c659f4e53
jonathan <jonathangalimberti@hotmail.com>
2026-05-15 15:32:07 -0300

    Responsividad, depuración de bugs y optimización de renderizado.

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const url = "./src/data/certificates.json";
 const container = document.getElementById("container");
 let certificates = [];
+let filteredCertificates = [];
 const home = document.getElementById("home");
 const certificatesSection = document.getElementById("certificatesSection");
 const main = document.getElementById("main");
+const sections = document.getElementById("sections")
 
 function toggleSection() {
   home.classList.toggle("d-none");
   certificatesSection.classList.toggle("d-none");
+  sections.classList.toggle("d-none");
+  console.log("cambiamos de lugar");
+
 }
 
-function goToHome(){
-    if(home.classList.contains("d-none")){
-        toggleSection()
-    }
+function goToHome() {
+  if (home.classList.contains("d-none")) {
+    toggleSection();
+   
+  }
+}
+
+function downloadCv(route, name) {
+  const link = document.createElement("a");
+  link.href = route;
+  link.download = name;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
 
 async function fetchData() {
@@ -32,30 +47,31 @@ fetchData();
 function printCertificates(data) {
   container.innerHTML = "";
   data.forEach((cert) => {
-    container.innerHTML += `
-    <div class="col-5 col-md-3">
-        <div class="card overflow-visible card-cert card-grow">
-        <img class="card-img-top" src="${cert.img}" alt="${cert.name}" />
-         <div class="overlay">${cert.name}</div>
-        </div>
-    </div>
-    `;
+    const card = document.createElement("div");
+    card.className = "col-5 col-md-3";
+    const cardBody = document.createElement("div");
+    cardBody.className = "card overflow-visible card-grow card-cert";
+    const cardImg = document.createElement("img");
+    cardImg.className = "card-img-top";
+    cardImg.src = cert.img;
+    cardImg.alt = cert.name;
+    const cardText = document.createElement("div");
+    cardText.className = "overlay";
+    cardText.textContent = cert.name;
+
+    card.appendChild(cardBody);
+    cardBody.appendChild(cardImg);
+    cardBody.appendChild(cardText);
+    container.appendChild(card);
   });
 }
 
 function filter(data) {
-  container.innerHTML = "";
-
+  filteredCertificates = [];
   certificates.forEach((cert) => {
     if (cert.category == data) {
-      container.innerHTML += `
-      <div class="col-5 col-md-3">
-        <div class="card overflow-visible card-grow card-cert">
-        <img class="card-img-top" src="${cert.img}" alt="${cert.name}" />
-        <div class="overlay">${cert.name}</div>
-        </div>
-    </div>
-    `;
+      filteredCertificates.push(cert);
+      printCertificates(filteredCertificates);
     } else if (data === "All") {
       printCertificates(certificates);
     }
@@ -63,24 +79,16 @@ function filter(data) {
 }
 
 function searchCertificate(value) {
-  container.innerHTML = "";
+  filteredCertificates = [];
   let valueString = value.toLowerCase();
-  
 
-  for (let i = 0; i < certificates.length; i++) {
-    const element = certificates[i];
-    if (element.name.toLowerCase().includes(valueString)) {
-      container.innerHTML += `
-      <div class="col-5 col-md-3">
-        <div class="card overflow-visible card-grow card-cert">
-        <img class="card-img-top" src="${element.img}" alt="${element.name}" />
-        <div class="overlay">${element.name}</div>
-        </div>
-    </div>
-    `;
+  certificates.forEach((cert) => {
+    if (cert.name.toLowerCase().includes(valueString)) {
+      filteredCertificates.push(cert);
+      printCertificates(filteredCertificates);
     }
-    else if(!element.name.toLowerCase().includes(valueString)){
-  container.innerHTML = `<p class ="text-white text-center fs-1">No certificates found</p>`;
+    if (filteredCertificates.length == 0) {
+      container.innerHTML = `<p class ="text-white text-center fs-1">No certificates found</p>`;
     }
-  }
+  });
 }
